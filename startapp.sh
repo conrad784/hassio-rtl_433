0ea83104334c2ab3ea1307b30654d7ccb54c52f1
Conrad Sachweh <conrad@csachweh.de>
2020-12-22 14:25:16 +0100

    remove unneeded hard-coded ppm value

--- startapp.sh
+++ startapp.sh
@@ -2,4 +2,4 @@
 
 python3 rtl_433_mqtt_hass.py -H ${MQTT_HOST} -p ${MQTT_PORT} -R rtl_433/+/events &
 
-rtl_433 -p 37 -C si -M protocol -M newmodel -F "mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USERNAME},pass=${MQTT_PASSWORD},retain=0,devices=rtl_433[/id]/devices[/model][/subtype][/channel]/[id],events=rtl_433/[id]/events" $@
+rtl_433 -C si -M protocol -M newmodel -F "mqtt://${MQTT_HOST}:${MQTT_PORT},user=${MQTT_USERNAME},pass=${MQTT_PASSWORD},retain=0,devices=rtl_433[/id]/devices[/model][/subtype][/channel]/[id],events=rtl_433/[id]/events" $@
